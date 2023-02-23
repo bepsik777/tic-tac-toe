04ce36cf713de9099a9265e580a9b2a9fa8311dd
bepsik777 <potocki.alex@gmail.com>
2023-02-23 10:43:59 +0100

    Create basic gameLogic module

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
 const gameBoard = (() => {
   const array = [
-    { a1: '', a2: '', a3: '' },
-    { b1: '', b2: '', b3: '' },
-    { c1: '', c2: '', c3: '' },
-  ];
+    'x', 'x', 'x',
+    'o', 'o', 'x',
+    'x', 'x', 'o'];
   return { array };
 })();
 
+
+
 const player = (symbol) => {
-  const makeMove = (object, row, field) => {
-    object.array[row][field] = symbol;
-    console.log(object);
+  this.symbol = symbol;
+  const makeMove = (field) => {
+    gameBoard.array[field] = symbol;
+    // How to use gameLogic.checkForWinner when it was declared later in the code?
+    console.log(gameBoard);
   };
   return { makeMove };
 };
 
+
+
 const gameLogic = (() => {
-  const startGame = (symbol) => {
-    const playerOnePrompt = prompt('X or O?');
-    const playerOne = player(playerOnePrompt);
-    let playerTwo;
-    if (playerOnePrompt === 'O') {
-      playerTwo = player('X');
-    } else if (playerOnePrompt === 'X') {
-      playerTwo = player('O');
+  let isThereWinner = false;
+  let winner;
+
+  function createPlayers() {
+    const playerOneSymbol = prompt('X or O?');
+    this.playerOne = player(playerOneSymbol);
+    if (playerOneSymbol === 'X') {
+      this.playerTwo = player('O');
+    } else if (playerOneSymbol === 'O') {
+      this.playerTwo = player('X');
     }
+  }
+
+  const winningOptions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkForWinner = () => {
+    winningOptions.forEach((option) => {
+      let xCount = 0;
+      let oCount = 0;
+      option.forEach((cell) => {
+        if (gameBoard.array[cell] === 'X') xCount += 1;
+        if (gameBoard.array[cell] === 'O') oCount += 1;
+      });
+      if (xCount === 3) {
+        isThereWinner = true;
+        winner = gameLogic.playerOne;
+      } else if (oCount === 3) {
+        isThereWinner = true;
+        winner = gameLogic.playerTwo;
+      }
+    });
   };
 
-  return { startGame };
+  return {
+    createPlayers,
+    checkForWinner,
+    isThereWinner,
+    winner,
+  };
 })();
+
+gameLogic.checkForWinner();
